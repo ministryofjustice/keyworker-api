538db529e66890256b39709e8549344427b06f85
Michael Willis <michael.willis@syscon.net>
2018-01-30 16:56:24 +0000

    Add JWT support and Passthrough correlation ID and token to downstream services

--- src/main/java/uk/gov/justice/digital/hmpps/keyworker/KeyworkerApplication.java
+++ src/main/java/uk/gov/justice/digital/hmpps/keyworker/KeyworkerApplication.java
@@ -3,12 +3,19 @@ package uk.gov.justice.digital.hmpps.keyworker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import uk.gov.justice.digital.hmpps.keyworker.utils.UserContextInterceptor;
 
 import java.security.Principal;
+import java.util.Collections;
+import java.util.List;
 
 @SpringBootApplication
 @EnableResourceServer
@@ -22,6 +29,20 @@ public class KeyworkerApplication {
         return "Hello " + user.getName();
     }
 
+    @Primary
+    @Bean
+    public RestTemplate getCustomRestTemplate() {
+        RestTemplate template = new RestTemplate();
+        List<ClientHttpRequestInterceptor> interceptors = template.getInterceptors();
+        if (interceptors == null) {
+            template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+        } else {
+            interceptors.add(new UserContextInterceptor());
+            template.setInterceptors(interceptors);
+        }
+
+        return template;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(KeyworkerApplication.class, args);
