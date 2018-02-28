64581db72b8fa18938e0b75e855302e769e41c4b
Michael Willis <michael.willis@syscon.net>
2018-02-28 06:54:36 +0000

    Add API Gateway token gen support (WIP)

--- src/main/java/uk/gov/justice/digital/hmpps/keyworker/utils/UserContext.java
+++ src/main/java/uk/gov/justice/digital/hmpps/keyworker/utils/UserContext.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserContext {
     public static final String CORRELATION_ID = "correlation-id";
-    public static final String AUTH_TOKEN     = "Authorization";
 
     private static final ThreadLocal<String> correlationId= new ThreadLocal<>();
     private static final ThreadLocal<String> authToken= new ThreadLocal<>();
