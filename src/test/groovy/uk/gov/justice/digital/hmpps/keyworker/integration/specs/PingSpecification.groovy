48252da5ec00e649246526a041cb6ea209602b33
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2019-06-26 10:55:50 +0100

    DT-8: Updated /ping to produce plain/text

--- src/test/groovy/uk/gov/justice/digital/hmpps/keyworker/integration/specs/PingSpecification.groovy
+++ src/test/groovy/uk/gov/justice/digital/hmpps/keyworker/integration/specs/PingSpecification.groovy
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.keyworker.integration.specs
 
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
+import org.springframework.util.MimeType
 
 class PingSpecification extends TestSpecification {
 
@@ -13,6 +14,7 @@ class PingSpecification extends TestSpecification {
 
         then:
         response.statusCode == HttpStatus.OK
+        response.headers.getContentType() == MimeType.valueOf("text/plain;charset=UTF-8")
         response.body == 'pong'
     }
 }
