49fab6d4acd1cf54b012e7d6e8fa14d835a95b7d
Peter Phillips <peter@greenthistle.co.uk>
2020-03-27 11:27:58 +0000

    DT-685: Don't verify signed token for telemetry

--- src/main/java/uk/gov/justice/digital/hmpps/keyworker/config/ClientTrackingTelemetryModule.java
+++ src/main/java/uk/gov/justice/digital/hmpps/keyworker/config/ClientTrackingTelemetryModule.java
@@ -4,33 +4,22 @@ import com.microsoft.applicationinsights.TelemetryConfiguration;
 import com.microsoft.applicationinsights.extensibility.TelemetryModule;
 import com.microsoft.applicationinsights.web.extensibility.modules.WebTelemetryModule;
 import com.microsoft.applicationinsights.web.internal.ThreadContext;
+import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import java.security.PublicKey;
 import java.text.ParseException;
 import java.util.Optional;
 
 @Slf4j
 @Configuration
 public class ClientTrackingTelemetryModule implements WebTelemetryModule, TelemetryModule {
-    private final PublicKeySupplier publicKeySupplier;
-
-    @Autowired
-    public ClientTrackingTelemetryModule(final PublicKeySupplier publicKeySupplier) {
-        this.publicKeySupplier = publicKeySupplier;
-    }
-
     @Override
     public void onBeginRequest(final ServletRequest req, final ServletResponse res) {
 
@@ -44,31 +33,19 @@ public class ClientTrackingTelemetryModule implements WebTelemetryModule, Teleme
 
                 final var properties = ThreadContext.getRequestTelemetryContext().getHttpRequestTelemetry().getProperties();
 
-                final var user = Optional.ofNullable(jwtBody.get("user_name"));
+                final var user = Optional.ofNullable(jwtBody.getClaim("user_name"));
                 user.map(String::valueOf).ifPresent(u -> properties.put("username", u));
 
-                properties.put("clientId", String.valueOf(jwtBody.get("client_id")));
-
-            } catch (final ExpiredJwtException e) {
-                // Expired token which spring security will handle
-            } catch (final Exception e) {
+                properties.put("clientId", String.valueOf(jwtBody.getClaim("client_id")));
+            } catch (final ParseException e) {
                 log.warn("problem decoding jwt public key for application insights", e);
             }
         }
     }
 
-    private Claims getClaimsFromJWT(final String token) throws Exception {
-
-        return Jwts.parser()
-                .setSigningKey(getPublicKey(token))
-                .parseClaimsJws(token.substring(7))
-                .getBody();
-    }
-
-    private PublicKey getPublicKey(final String token) throws ParseException {
+    private JWTClaimsSet getClaimsFromJWT(final String token) throws ParseException {
         final var signedJWT = SignedJWT.parse(token.replace("Bearer ", ""));
-        final var kid = signedJWT.getHeader().getKeyID();
-        return publicKeySupplier.getPublicKeyForKeyId(kid);
+        return signedJWT.getJWTClaimsSet();
     }
 
     @Override
--- src/main/java/uk/gov/justice/digital/hmpps/keyworker/config/PublicKeySupplier.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.digital.hmpps.keyworker.config;
-
-import java.security.PublicKey;
-
-public interface PublicKeySupplier {
-
-    PublicKey getPublicKeyForKeyId(String keyId);
-}
