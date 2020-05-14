72df176b0e7cb623df5534998b30db9c30847492
Peter Phillips <peter@greenthistle.co.uk>
2020-05-14 07:43:49 +0100

    DT-846: Add back in the configuration processor

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ configurations {
 extra["spring-security.version"] = "5.3.2.RELEASE" // Updated since spring-boot-starter-oauth2-resource-server-2.2.5.RELEASE only pulls in 5.2.2.RELEASE (still affected by CVE-2018-1258 though)
 
 dependencies {
+  annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
   annotationProcessor("org.projectlombok:lombok:1.18.12")
   testAnnotationProcessor("org.projectlombok:lombok:1.18.12")
 
