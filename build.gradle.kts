b4c3bc6f33f480f962c029ba9788a9cc740dc9b2
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2020-06-17 15:47:32 +0100

    DT-851: updated logging and added version for app insights updated docker compose to pull images from quay switched Master to Main and upgraded springboot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "0.4.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "0.4.2"
+  kotlin("plugin.spring") version "1.3.72"
+  kotlin("plugin.jpa") version "1.3.72"
 }
 
 configurations {
