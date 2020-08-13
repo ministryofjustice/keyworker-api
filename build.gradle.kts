7f4c17ff55f480cd5e166588ff8472503d4721d0
Nevelina Aleksandrova <nevelina.aleksandrova@digital.justice.gov.uk>
2020-08-13 10:06:33 +0100

    NN-2870: Upgrade DPS gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "0.4.7"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "0.4.8"
   kotlin("plugin.spring") version "1.3.72"
   kotlin("plugin.jpa") version "1.3.72"
 }
