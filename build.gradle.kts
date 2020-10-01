b57fd515bcb4025401377d780deeebbb4b06981f
= <=>
2020-10-01 15:22:06 +0100

    NN-2965 use springfox boot starter

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,8 +37,9 @@ dependencies {
   implementation("javax.activation:activation:1.1.1")
   implementation("javax.transaction:javax.transaction-api:1.3")
 
-  implementation("io.springfox:springfox-swagger2:3.0.0")
-  implementation("io.springfox:springfox-swagger-ui:3.0.0")
+  implementation("io.springfox:springfox-boot-starter:3.0.0")
+/*  implementation("io.springfox:springfox-swagger2:3.0.0")
+  implementation("io.springfox:springfox-swagger-ui:3.0.0")*/
 
   implementation("net.sf.ehcache:ehcache:2.10.6")
   implementation("org.apache.commons:commons-text:1.9")
