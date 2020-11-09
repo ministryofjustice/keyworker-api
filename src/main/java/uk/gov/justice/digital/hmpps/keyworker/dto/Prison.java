5caf08bc6b11861c27ac3470038eb2f0f5fc275a
James King <james.king@digital.justice.gov.uk>
2020-11-09 18:11:22 +0000

    NN-3025: convert to kotlin: KeyworkerServiceController

--- src/main/java/uk/gov/justice/digital/hmpps/keyworker/dto/Prison.java
+++ src/main/java/uk/gov/justice/digital/hmpps/keyworker/dto/Prison.java
@@ -3,23 +3,13 @@ package uk.gov.justice.digital.hmpps.keyworker.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @ApiModel(description = "Prison")
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@EqualsAndHashCode(of = "prisonId")
 public class Prison {
     @ApiModelProperty(required = true, value = "Identifies prison.", example = "MDI", position = 0)
     @NotBlank
@@ -53,4 +43,175 @@ public class Prison {
     @NotBlank
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime migratedDateTime;
+
+    public Prison(@NotBlank String prisonId, @NotBlank boolean supported, @NotBlank boolean migrated, @NotBlank boolean autoAllocatedSupported, @NotBlank int capacityTier1, @NotBlank int capacityTier2, @NotBlank int kwSessionFrequencyInWeeks, @NotBlank LocalDateTime migratedDateTime) {
+        this.prisonId = prisonId;
+        this.supported = supported;
+        this.migrated = migrated;
+        this.autoAllocatedSupported = autoAllocatedSupported;
+        this.capacityTier1 = capacityTier1;
+        this.capacityTier2 = capacityTier2;
+        this.kwSessionFrequencyInWeeks = kwSessionFrequencyInWeeks;
+        this.migratedDateTime = migratedDateTime;
+    }
+
+    public Prison() {
+    }
+
+    public static PrisonBuilder builder() {
+        return new PrisonBuilder();
+    }
+
+    public @NotBlank String getPrisonId() {
+        return this.prisonId;
+    }
+
+    public @NotBlank boolean isSupported() {
+        return this.supported;
+    }
+
+    public @NotBlank boolean isMigrated() {
+        return this.migrated;
+    }
+
+    public @NotBlank boolean isAutoAllocatedSupported() {
+        return this.autoAllocatedSupported;
+    }
+
+    public @NotBlank int getCapacityTier1() {
+        return this.capacityTier1;
+    }
+
+    public @NotBlank int getCapacityTier2() {
+        return this.capacityTier2;
+    }
+
+    public @NotBlank int getKwSessionFrequencyInWeeks() {
+        return this.kwSessionFrequencyInWeeks;
+    }
+
+    public @NotBlank LocalDateTime getMigratedDateTime() {
+        return this.migratedDateTime;
+    }
+
+    public void setPrisonId(@NotBlank String prisonId) {
+        this.prisonId = prisonId;
+    }
+
+    public void setSupported(@NotBlank boolean supported) {
+        this.supported = supported;
+    }
+
+    public void setMigrated(@NotBlank boolean migrated) {
+        this.migrated = migrated;
+    }
+
+    public void setAutoAllocatedSupported(@NotBlank boolean autoAllocatedSupported) {
+        this.autoAllocatedSupported = autoAllocatedSupported;
+    }
+
+    public void setCapacityTier1(@NotBlank int capacityTier1) {
+        this.capacityTier1 = capacityTier1;
+    }
+
+    public void setCapacityTier2(@NotBlank int capacityTier2) {
+        this.capacityTier2 = capacityTier2;
+    }
+
+    public void setKwSessionFrequencyInWeeks(@NotBlank int kwSessionFrequencyInWeeks) {
+        this.kwSessionFrequencyInWeeks = kwSessionFrequencyInWeeks;
+    }
+
+    public void setMigratedDateTime(@NotBlank LocalDateTime migratedDateTime) {
+        this.migratedDateTime = migratedDateTime;
+    }
+
+    public String toString() {
+        return "Prison(prisonId=" + this.getPrisonId() + ", supported=" + this.isSupported() + ", migrated=" + this.isMigrated() + ", autoAllocatedSupported=" + this.isAutoAllocatedSupported() + ", capacityTier1=" + this.getCapacityTier1() + ", capacityTier2=" + this.getCapacityTier2() + ", kwSessionFrequencyInWeeks=" + this.getKwSessionFrequencyInWeeks() + ", migratedDateTime=" + this.getMigratedDateTime() + ")";
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Prison)) return false;
+        final Prison other = (Prison) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$prisonId = this.getPrisonId();
+        final Object other$prisonId = other.getPrisonId();
+        if (this$prisonId == null ? other$prisonId != null : !this$prisonId.equals(other$prisonId)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Prison;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $prisonId = this.getPrisonId();
+        result = result * PRIME + ($prisonId == null ? 43 : $prisonId.hashCode());
+        return result;
+    }
+
+    public static class PrisonBuilder {
+        private @NotBlank String prisonId;
+        private @NotBlank boolean supported;
+        private @NotBlank boolean migrated;
+        private @NotBlank boolean autoAllocatedSupported;
+        private @NotBlank int capacityTier1;
+        private @NotBlank int capacityTier2;
+        private @NotBlank int kwSessionFrequencyInWeeks;
+        private @NotBlank LocalDateTime migratedDateTime;
+
+        PrisonBuilder() {
+        }
+
+        public Prison.PrisonBuilder prisonId(@NotBlank String prisonId) {
+            this.prisonId = prisonId;
+            return this;
+        }
+
+        public Prison.PrisonBuilder supported(@NotBlank boolean supported) {
+            this.supported = supported;
+            return this;
+        }
+
+        public Prison.PrisonBuilder migrated(@NotBlank boolean migrated) {
+            this.migrated = migrated;
+            return this;
+        }
+
+        public Prison.PrisonBuilder autoAllocatedSupported(@NotBlank boolean autoAllocatedSupported) {
+            this.autoAllocatedSupported = autoAllocatedSupported;
+            return this;
+        }
+
+        public Prison.PrisonBuilder capacityTier1(@NotBlank int capacityTier1) {
+            this.capacityTier1 = capacityTier1;
+            return this;
+        }
+
+        public Prison.PrisonBuilder capacityTier2(@NotBlank int capacityTier2) {
+            this.capacityTier2 = capacityTier2;
+            return this;
+        }
+
+        public Prison.PrisonBuilder kwSessionFrequencyInWeeks(@NotBlank int kwSessionFrequencyInWeeks) {
+            this.kwSessionFrequencyInWeeks = kwSessionFrequencyInWeeks;
+            return this;
+        }
+
+        public Prison.PrisonBuilder migratedDateTime(@NotBlank LocalDateTime migratedDateTime) {
+            this.migratedDateTime = migratedDateTime;
+            return this;
+        }
+
+        public Prison build() {
+            return new Prison(prisonId, supported, migrated, autoAllocatedSupported, capacityTier1, capacityTier2, kwSessionFrequencyInWeeks, migratedDateTime);
+        }
+
+        public String toString() {
+            return "Prison.PrisonBuilder(prisonId=" + this.prisonId + ", supported=" + this.supported + ", migrated=" + this.migrated + ", autoAllocatedSupported=" + this.autoAllocatedSupported + ", capacityTier1=" + this.capacityTier1 + ", capacityTier2=" + this.capacityTier2 + ", kwSessionFrequencyInWeeks=" + this.kwSessionFrequencyInWeeks + ", migratedDateTime=" + this.migratedDateTime + ")";
+        }
+    }
 }
--- src/test/java/uk/gov/justice/digital/hmpps/keyworker/controllers/KeyworkerServiceControllerTest.kt
+++ src/test/java/uk/gov/justice/digital/hmpps/keyworker/controllers/KeyworkerServiceControllerTest.kt
@@ -2,6 +2,8 @@ package uk.gov.justice.digital.hmpps.keyworker.controllers
 
 import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
+import com.nhaarman.mockitokotlin2.any
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import org.springframework.beans.factory.annotation.Autowired
@@ -14,13 +16,19 @@ import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.web.reactive.function.client.WebClientResponseException
+import uk.gov.justice.digital.hmpps.keyworker.dto.AllocationsFilterDto
 import uk.gov.justice.digital.hmpps.keyworker.dto.BasicKeyworkerDto
+import uk.gov.justice.digital.hmpps.keyworker.dto.KeyworkerAllocationDetailsDto
+import uk.gov.justice.digital.hmpps.keyworker.dto.Page
+import uk.gov.justice.digital.hmpps.keyworker.dto.PagingAndSortingDto
+import uk.gov.justice.digital.hmpps.keyworker.dto.SortOrder
 import uk.gov.justice.digital.hmpps.keyworker.rolemigration.UserRolesMigrationService
 import uk.gov.justice.digital.hmpps.keyworker.services.KeyworkerAutoAllocationService
 import uk.gov.justice.digital.hmpps.keyworker.services.KeyworkerMigrationService
 import uk.gov.justice.digital.hmpps.keyworker.services.KeyworkerService
 import uk.gov.justice.digital.hmpps.keyworker.services.PrisonSupportedService
-import java.util.*
+import java.time.LocalDate
+import java.util.Optional
 
 @WebFluxTest(KeyworkerServiceController::class)
 @ActiveProfiles("test")
@@ -28,50 +36,89 @@ import java.util.*
 @WithMockUser
 class KeyworkerServiceControllerTest() {
 
-  @Autowired
-  private lateinit var webTestClient: WebTestClient
+    @Autowired
+    private lateinit var webTestClient: WebTestClient
 
-  @MockBean
-  private lateinit var keyworkerService: KeyworkerService
-  @MockBean
-  private lateinit var keyworkerMigrationService: KeyworkerMigrationService
-  @MockBean
-  private lateinit var roleMigrationService: UserRolesMigrationService
-  @MockBean
-  private lateinit var keyworkerAutoAllocationService: KeyworkerAutoAllocationService
-  @MockBean
-  private lateinit var prisonSupportedService: PrisonSupportedService
+    @MockBean
+    private lateinit var keyworkerService: KeyworkerService
+    @MockBean
+    private lateinit var keyworkerMigrationService: KeyworkerMigrationService
+    @MockBean
+    private lateinit var roleMigrationService: UserRolesMigrationService
+    @MockBean
+    private lateinit var keyworkerAutoAllocationService: KeyworkerAutoAllocationService
+    @MockBean
+    private lateinit var prisonSupportedService: PrisonSupportedService
 
+    @ParameterizedTest
+    @ValueSource(strings = arrayOf("/key-worker/LEI", "/key-worker")) // Includes deprecated version of the API
+    fun `offender keyworker found should return ok`(pathPrefix: String) {
 
-  @ParameterizedTest
-  @ValueSource(strings = arrayOf("/key-worker/LEI", "/key-worker")) // Includes deprecated version of the API
-  fun `offender keyworker found should return ok`(pathPrefix: String) {
+        whenever(keyworkerService.getCurrentKeyworkerForPrisoner("A1234AA"))
+            .thenReturn(Optional.of(BasicKeyworkerDto.builder().build()))
 
-    whenever(keyworkerService.getCurrentKeyworkerForPrisoner("A1234AA"))
-        .thenReturn(Optional.of(BasicKeyworkerDto.builder().build()))
+        webTestClient.get()
+            .uri("$pathPrefix/offender/A1234AA")
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isOk
 
-    webTestClient.get()
-        .uri("$pathPrefix/offender/A1234AA")
-        .accept(MediaType.APPLICATION_JSON)
-        .exchange()
-        .expectStatus().isOk
+        verify(keyworkerService).getCurrentKeyworkerForPrisoner("A1234AA")
+    }
 
-    verify(keyworkerService).getCurrentKeyworkerForPrisoner("A1234AA")
-  }
+    @ParameterizedTest
+    @ValueSource(strings = arrayOf("/key-worker/LEI", "/key-worker")) // Includes deprecated version of the API
+    fun `offender keyworker not found should return not found`(pathPrefix: String) {
 
-  @ParameterizedTest
-  @ValueSource(strings = arrayOf("/key-worker/LEI", "/key-worker")) // Includes deprecated version of the API
-  fun `offender keyworker not found should return not found`(pathPrefix: String) {
+        whenever(keyworkerService.getCurrentKeyworkerForPrisoner("A1234AA"))
+            .thenThrow(WebClientResponseException.create(404, "Not Found", HttpHeaders.EMPTY, null, null, null))
 
-    whenever(keyworkerService.getCurrentKeyworkerForPrisoner("A1234AA"))
-        .thenThrow(WebClientResponseException.create(404, "Not Found", HttpHeaders.EMPTY, null, null, null))
+        webTestClient.get()
+            .uri("$pathPrefix/offender/A1234AA")
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isNotFound
 
-    webTestClient.get()
-        .uri("$pathPrefix/offender/A1234AA")
-        .accept(MediaType.APPLICATION_JSON)
-        .exchange()
-        .expectStatus().isNotFound
+        verify(keyworkerService).getCurrentKeyworkerForPrisoner("A1234AA")
+    }
 
-    verify(keyworkerService).getCurrentKeyworkerForPrisoner("A1234AA")
-  }
+    @Test
+    fun `correct default values are passed to getAllocations`() {
+        val prisonId = "123"
+
+        val page = Page<KeyworkerAllocationDetailsDto>(emptyList(), defaultHttpHeaders())
+        whenever(keyworkerService.getAllocations(any(), any())).thenReturn(page)
+
+        webTestClient.get()
+            .uri("/key-worker/$prisonId/allocations")
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isOk
+
+        val expectedFilterDto = AllocationsFilterDto
+            .builder()
+            .prisonId(prisonId)
+            .fromDate(Optional.empty())
+            .toDate(LocalDate.now())
+            .allocationType(Optional.empty())
+            .build()
+
+        val expectedPagingDto = PagingAndSortingDto
+            .builder()
+            .pageOffset(0)
+            .pageLimit(10)
+            .sortOrder(SortOrder.ASC)
+            .sortFields("")
+            .build()
+
+        verify(keyworkerService).getAllocations(expectedFilterDto, expectedPagingDto)
+    }
+
+    private fun defaultHttpHeaders(): HttpHeaders {
+        val httpHeaders = HttpHeaders()
+        httpHeaders.set(Page.HEADER_TOTAL_RECORDS, "0")
+        httpHeaders.set(Page.HEADER_PAGE_OFFSET, "0")
+        httpHeaders.set(Page.HEADER_PAGE_LIMIT, "0")
+        return httpHeaders
+    }
 }
