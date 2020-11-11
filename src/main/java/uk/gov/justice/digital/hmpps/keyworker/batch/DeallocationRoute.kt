b29418f3a177f1e0c8db0777692a987557e97a43
James King <james.king@digital.justice.gov.uk>
2020-11-11 15:21:05 +0000

    NN-3025: convert 4 batch classes

---
+++ src/main/java/uk/gov/justice/digital/hmpps/keyworker/batch/DeallocationRoute.kt
@@ -0,0 +1,29 @@
+package uk.gov.justice.digital.hmpps.keyworker.batch
+
+import org.apache.camel.builder.RouteBuilder
+import org.apache.commons.lang3.StringUtils
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.stereotype.Component
+
+/**
+ * A Scheduled job that deallocates prisoners that have moved to another prison or have been released.
+ */
+@Component
+@ConditionalOnProperty(name = ["quartz.enabled"])
+class DeallocationRoute : RouteBuilder() {
+
+  @Value("\${deallocation.job.cron}")
+  private val cronExpression: String? = null
+
+  override fun configure() {
+    if (StringUtils.isNotBlank(cronExpression)) {
+      from(QUARTZ_UPDATE_STATUS_URI + cronExpression)
+        .log("*** DEPRECATED: removed de-allocation process")
+    }
+  }
+
+  companion object {
+    private const val QUARTZ_UPDATE_STATUS_URI = "quartz2://application/deallocation?cron="
+  }
+}
