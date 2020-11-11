a86d35ab3f5d4f6c1e4201a5e10f1a2dd6313d02
jamesak-oncloud9 <72737963+jamesak-oncloud9@users.noreply.github.com>
2020-11-11 13:13:42 +0000

    NN-3025-3: convert security files (#275)
    * NN-3025: convert to kotlin: KeyworkerServiceController
    
    * NN-3025: reformat code
    
    * NN-3025: refactor code
    
    * Rename .java to .kt
    
    * NN-3NN-3025-DS Converted Java to kotlin
    
    * NN-3025: convert 2 security files
    
    * Rename .java to .kt
    
    * NN-3NN-3025-DS Converted Java to kotlin
    
    * NN-3025: format code
    
    * NN-3025: convert MethodSecurityConfiguration
    
    * NN-3025: code formatting
    
    Co-authored-by: dnyaneshsonwane <dnyanesh.sonwane"digital.justice.gov.uk>

---
+++ src/main/java/uk/gov/justice/digital/hmpps/keyworker/security/AuthAwareAuthenticationToken.kt
@@ -0,0 +1,18 @@
+package uk.gov.justice.digital.hmpps.keyworker.security
+
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
+
+internal class AuthAwareAuthenticationToken(jwt: Jwt, private val principal: Any?, authorities: Collection<GrantedAuthority>) : JwtAuthenticationToken(jwt, authorities) {
+
+  private val name: String = principal?.toString() ?: ""
+
+  override fun getPrincipal(): Any? {
+    return this.principal
+  }
+
+  override fun getName(): String {
+    return this.name
+  }
+}
--- src/main/java/uk/gov/justice/digital/hmpps/keyworker/security/MethodSecurityConfiguration.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.justice.digital.hmpps.keyworker.security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
-
-@Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
-public class MethodSecurityConfiguration extends GlobalMethodSecurityConfiguration {}
