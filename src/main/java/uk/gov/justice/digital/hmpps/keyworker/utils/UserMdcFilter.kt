567139ee260067ad63a312d8d3b8204417e5c706
jamesak-oncloud9 <72737963+jamesak-oncloud9@users.noreply.github.com>
2020-11-12 09:59:11 +0000

    NN-3025-5: convert filter classes (#278)
    * NN-3025: convert UserContextFilter
    
    * NN-3025: convert filter classes
    
    * NN-3025: remove annotation

---
+++ src/main/java/uk/gov/justice/digital/hmpps/keyworker/utils/UserMdcFilter.kt
@@ -0,0 +1,33 @@
+package uk.gov.justice.digital.hmpps.keyworker.utils
+
+import org.slf4j.MDC
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.keyworker.security.UserSecurityUtils
+import java.io.IOException
+import javax.servlet.Filter
+import javax.servlet.FilterChain
+import javax.servlet.ServletException
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+import kotlin.Throws
+
+@Component
+@Order(1)
+class UserMdcFilter(private val userSecurityUtils: UserSecurityUtils) : Filter {
+
+  @Throws(IOException::class, ServletException::class)
+  override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
+    val currentUsername = userSecurityUtils.currentUsername
+    try {
+      if (currentUsername != null) {
+        MDC.put(MdcUtility.USER_ID_HEADER, currentUsername)
+      }
+      chain.doFilter(request, response)
+    } finally {
+      if (currentUsername != null) {
+        MDC.remove(MdcUtility.USER_ID_HEADER)
+      }
+    }
+  }
+}
