65a3aecd9fe18aa1a33929f629958afc6496b7bb
jamesak-oncloud9 <72737963+jamesak-oncloud9@users.noreply.github.com>
2020-11-12 11:30:38 +0000

    NN-3025-6: convert batch classes (#279)
    * NN-3025: convert 4 batch classes
    
    * NN-3025: convert 2 more batch classes

---
+++ src/main/java/uk/gov/justice/digital/hmpps/keyworker/batch/CamelQuartzConfigurationFactory.kt
@@ -0,0 +1,103 @@
+package uk.gov.justice.digital.hmpps.keyworker.batch
+
+import org.apache.camel.CamelContext
+import org.apache.camel.ProducerTemplate
+import org.apache.camel.component.quartz2.QuartzComponent
+import org.apache.camel.spring.boot.CamelContextConfiguration
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.util.*
+
+/* requires postgres db - does not work with in memory database */
+@Configuration
+@ConditionalOnProperty(name = ["quartz.enabled"])
+internal class CamelQuartzConfigurationFactory {
+  @Autowired
+  private val camelContext: CamelContext? = null
+
+  @Value("\${org.quartz.jobStore.class}")
+  private val quartzJobstoreClass: String? = null
+
+  @Value("\${org.quartz.threadPool.threadCount}")
+  private val threadCount: String? = null
+
+  @Value("\${app.db.url}")
+  private val url: String? = null
+
+  @Value("\${spring.datasource.username}")
+  private val username: String? = null
+
+  @Value("\${spring.datasource.password}")
+  private val password: String? = null
+
+  @Value("\${org.quartz.jobStore.driverDelegateClass}")
+  private val quartzDelegateClass: String? = null
+
+  @Value("\${database.driver.class}")
+  private val databaseDriverClass: String? = null
+
+  @Value("\${org.quartz.jobStore.misfireThreshold}")
+  private val misfireThreshold: String? = null
+
+  @Value("\${org.quartz.jobStore.tablePrefix}")
+  private val tablePrefix: String? = null
+
+  @Value("\${org.quartz.jobStore.clusterCheckinInterval}")
+  private val clusterCheckinInterval: String? = null
+
+  @Value("\${org.quartz.jobStore.isClustered}")
+  private val isClustered: String? = null
+
+  @Value("\${org.quartz.scheduler.instanceName}")
+  private val instanceName: String? = null
+
+  @Value("\${org.quartz.scheduler.instanceId}")
+  private val instanceId: String? = null
+
+  @Bean
+  fun contextConfiguration(): CamelContextConfiguration {
+    return object : CamelContextConfiguration {
+      override fun beforeApplicationStart(context: CamelContext) {
+        context.addComponent("quartz2", quartz())
+        context.isUseMDCLogging = true
+      }
+
+      override fun afterApplicationStart(camelContext: CamelContext) {
+        // no changes after started required.
+      }
+    }
+  }
+
+  @Bean
+  fun producerTemplate(): ProducerTemplate {
+    return camelContext!!.createProducerTemplate()
+  }
+
+  @Bean
+  fun quartz(): QuartzComponent {
+    val quartz = QuartzComponent()
+    val properties = Properties()
+    properties["org.quartz.jobStore.dataSource"] = "applicationDS"
+    properties["org.quartz.jobStore.driverDelegateClass"] = quartzDelegateClass
+    properties["org.quartz.dataSource.applicationDS.driver"] = databaseDriverClass
+    properties["org.quartz.dataSource.applicationDS.URL"] = url
+    properties["org.quartz.dataSource.applicationDS.user"] = username
+    properties["org.quartz.dataSource.applicationDS.password"] = password
+    properties["org.quartz.jobStore.class"] = quartzJobstoreClass
+    properties["quartz.scheduler.instanceName"] = instanceName
+    properties["org.quartz.scheduler.instanceId"] = instanceId
+    properties["org.quartz.threadPool.class"] = "org.quartz.simpl.SimpleThreadPool"
+    properties["org.quartz.threadPool.threadCount"] = threadCount
+    properties["org.quartz.threadPool.threadPriority"] = "5"
+    properties["org.quartz.jobStore.misfireThreshold"] = misfireThreshold
+    properties["org.quartz.jobStore.useProperties"] = "false"
+    properties["org.quartz.jobStore.tablePrefix"] = tablePrefix
+    properties["org.quartz.jobStore.isClustered"] = isClustered
+    properties["org.quartz.jobStore.clusterCheckinInterval"] = clusterCheckinInterval
+    quartz.properties = properties
+    return quartz
+  }
+}
---
+++ src/main/java/uk/gov/justice/digital/hmpps/keyworker/batch/EnableNewNomisRoute.kt
@@ -0,0 +1,69 @@
+package uk.gov.justice.digital.hmpps.keyworker.batch
+
+import com.google.common.collect.ImmutableMap
+import com.microsoft.applicationinsights.TelemetryClient
+import org.apache.camel.Exchange
+import org.apache.camel.LoggingLevel
+import org.apache.camel.builder.RouteBuilder
+import org.apache.commons.lang3.StringUtils
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.keyworker.dto.CaseloadUpdate
+import uk.gov.justice.digital.hmpps.keyworker.services.NomisService
+
+/**
+ * A Scheduled job that checks builds stats for each prison for the previous day
+ */
+@Component
+@ConditionalOnProperty(name = ["quartz.enabled"])
+class EnableNewNomisRoute @Autowired constructor(private val nomisService: NomisService, private val telemetryClient: TelemetryClient) : RouteBuilder() {
+
+  @Value("\${enable-new-nomis.job.cron}")
+  private val cronExpression: String? = null
+
+  override fun configure() {
+
+    if (StringUtils.isNotBlank(cronExpression)) {
+      from(QUARTZ_ENABLE_NEW_NOMIS_URI + cronExpression)
+        .to(ENABLE_NEW_NOMIS)
+    }
+
+    from(ENABLE_NEW_NOMIS)
+      .log("Starting: Checking for new users and enabling user access to API")
+      .bean(nomisService, "getAllPrisons")
+      .log("There are \${body.size} prisons")
+      .split(body())
+      .to(ADD_NWEB_CASELOAD)
+      .end()
+      .log("Complete: Checking for new Users and Enabling User access to API")
+    from(ADD_NWEB_CASELOAD)
+      .errorHandler(deadLetterChannel(DLQ).logRetryAttempted(true).redeliveryDelay(5000).backOffMultiplier(0.29).maximumRedeliveries(2))
+      .setProperty("prisonId", simple("\${body.prisonId}"))
+      .log("Starting: Enabling API access for all active users in prison \${property.prisonId}")
+      .bean(nomisService, "enableNewNomisForCaseload(\${property.prisonId})")
+      .choice().`when`(simple("\${body.numUsersEnabled} > 0"))
+      .log("Enabled \${body.numUsersEnabled} new users for API access")
+      .process { p: Exchange ->
+        val caseloadRes = p.getIn().getBody(CaseloadUpdate::class.java)
+        val infoMap = ImmutableMap.of(
+          "prisonId", caseloadRes.caseload,
+          "numUsersEnabled", caseloadRes.numUsersEnabled.toString()
+        )
+        telemetryClient.trackEvent("ApiUsersEnabled", infoMap, null) // -
+      }
+      .end()
+      .log("Complete: Enabling API access for all active users in prison \${property.prisonId}")
+    from(DLQ)
+      .log(LoggingLevel.ERROR, "Error occurred processing \${body.prisonId}")
+      .to("log:stats-error?level=ERROR&showCaughtException=true&showStackTrace=true&showAll=true")
+  }
+
+  companion object {
+    const val ENABLE_NEW_NOMIS = "direct:enableNewNomis"
+    private const val QUARTZ_ENABLE_NEW_NOMIS_URI = "quartz2://application/enableNewNomis?cron="
+    private const val ADD_NWEB_CASELOAD = "direct:add-nweb-caseload"
+    private const val DLQ = "direct:dlq"
+  }
+}
