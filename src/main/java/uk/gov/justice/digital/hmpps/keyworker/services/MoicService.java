a0ba29b8ad99b1b853bfc6f1c8088a8a1cd88f2b
JamieDraper <65b174d7066dfab51fad25c9c8774bf6392b1610>
2021-01-14 12:46:55 +0000

    NN-3109 fix test and make use of set

--- src/main/java/uk/gov/justice/digital/hmpps/keyworker/services/MoicService.java
+++ src/main/java/uk/gov/justice/digital/hmpps/keyworker/services/MoicService.java
@@ -6,16 +6,17 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
 public class MoicService {
     @Value("${womens.estate.complex.offenders}")
-    final List<String> complexOffenders;
+    final Set<String> complexOffenders;
 
     @Value("${womens.estate}")
-    final List<String> womensEstatePrisons;
+    final Set<String> womensEstatePrisons;
 
     public List<String> getComplexOffenders(final String prisonId, final List<String> offenderNos) {
         if(!womensEstatePrisons.contains(prisonId)) return Collections.emptyList();
