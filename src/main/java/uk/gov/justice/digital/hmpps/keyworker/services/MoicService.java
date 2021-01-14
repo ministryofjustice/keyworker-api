527595388567fc78ae075ed12553ebc4c2bf1ca4
JamieDraper <65b174d7066dfab51fad25c9c8774bf6392b1610>
2021-01-14 12:38:40 +0000

    NN-3109 use the correct constructor param type

--- src/main/java/uk/gov/justice/digital/hmpps/keyworker/services/MoicService.java
+++ src/main/java/uk/gov/justice/digital/hmpps/keyworker/services/MoicService.java
@@ -1,11 +1,9 @@
 package uk.gov.justice.digital.hmpps.keyworker.services;
 
 import lombok.AllArgsConstructor;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,24 +12,16 @@ import java.util.stream.Collectors;
 @Service
 public class MoicService {
     @Value("${womens.estate.complex.offenders}")
-    final String complexOffenders;
+    final List<String> complexOffenders;
 
     @Value("${womens.estate}")
-    final String womensEstatePrisons;
+    final List<String> womensEstatePrisons;
 
     public List<String> getComplexOffenders(final String prisonId, final List<String> offenderNos) {
-        if(!getEnabledPrisons().contains(prisonId)) return Collections.emptyList();
-
-        final var complex = Arrays.stream(complexOffenders.split(",")).toArray();
+        if(!womensEstatePrisons.contains(prisonId)) return Collections.emptyList();
 
         return offenderNos.stream()
-            .filter(offenderNo -> Arrays.asList(complex).contains(offenderNo))
+            .filter(complexOffenders::contains)
             .collect(Collectors.toList());
-    }
-
-    private List<String> getEnabledPrisons() {
-        if(StringUtils.isBlank(womensEstatePrisons)) return Collections.emptyList();
-
-        return Arrays.stream(womensEstatePrisons.split(",")).collect(Collectors.toList());
     }
 }
