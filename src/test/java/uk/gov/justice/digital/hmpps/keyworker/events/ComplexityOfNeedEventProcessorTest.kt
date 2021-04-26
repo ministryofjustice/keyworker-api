a8dcf5b5f63951e1699610617844283b1b0eb986
JamieDraper <65b174d7066dfab51fad25c9c8774bf6392b1610>
2021-04-26 10:24:47 +0100

    NN-3344: - Swallow entity not found exceptions triggered by complexity events

--- src/test/java/uk/gov/justice/digital/hmpps/keyworker/events/ComplexityOfNeedEventProcessorTest.kt
+++ src/test/java/uk/gov/justice/digital/hmpps/keyworker/events/ComplexityOfNeedEventProcessorTest.kt
@@ -2,18 +2,17 @@ package uk.gov.justice.digital.hmpps.keyworker.events
 
 import com.google.gson.Gson
 import com.microsoft.applicationinsights.TelemetryClient
-import com.nhaarman.mockito_kotlin.any
-import com.nhaarman.mockito_kotlin.never
-import com.nhaarman.mockito_kotlin.times
-import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.ArgumentMatchers.anyString
 import org.mockito.Mock
 import org.mockito.Mockito
+import org.mockito.Mockito.lenient
 import org.mockito.junit.jupiter.MockitoExtension
 import uk.gov.justice.digital.hmpps.keyworker.config.JsonConfig
 import uk.gov.justice.digital.hmpps.keyworker.services.KeyworkerService
+import javax.persistence.EntityNotFoundException
 
 @ExtendWith(MockitoExtension::class)
 class ComplexityOfNeedEventProcessorTest {
@@ -37,7 +36,8 @@ class ComplexityOfNeedEventProcessorTest {
 
   @Test
   fun `should not deallocate offenders that do not have high complexity of needs`() {
-    complexityOfNeedEventProcessor = ComplexityOfNeedEventProcessor(keyworkerService, telemetryClient, gson, "http://local")
+    complexityOfNeedEventProcessor =
+      ComplexityOfNeedEventProcessor(keyworkerService, telemetryClient, gson, "http://local")
     complexityOfNeedEventProcessor.onComplexityChange(COMPLEXITY_MESSAGE_LOW)
 
     verify(keyworkerService, never()).deallocate(OFFENDER_NO)
@@ -45,7 +45,8 @@ class ComplexityOfNeedEventProcessorTest {
 
   @Test
   fun `should deallocate offenders that have high complexity of needs`() {
-    complexityOfNeedEventProcessor = ComplexityOfNeedEventProcessor(keyworkerService, telemetryClient, gson, "http://local")
+    complexityOfNeedEventProcessor =
+      ComplexityOfNeedEventProcessor(keyworkerService, telemetryClient, gson, "http://local")
     complexityOfNeedEventProcessor.onComplexityChange(COMPLEXITY_MESSAGE_HIGH)
 
     verify(keyworkerService, times(1)).deallocate(OFFENDER_NO)
@@ -53,7 +54,8 @@ class ComplexityOfNeedEventProcessorTest {
 
   @Test
   fun `should raise a telemetry event`() {
-    complexityOfNeedEventProcessor = ComplexityOfNeedEventProcessor(keyworkerService, telemetryClient, gson, "http://local")
+    complexityOfNeedEventProcessor =
+      ComplexityOfNeedEventProcessor(keyworkerService, telemetryClient, gson, "http://local")
     complexityOfNeedEventProcessor.onComplexityChange(COMPLEXITY_MESSAGE_MEDIUM)
 
     verify(telemetryClient, Mockito.times(1)).trackEvent(
@@ -70,5 +72,15 @@ class ComplexityOfNeedEventProcessorTest {
 
     verify(keyworkerService, never()).deallocate(OFFENDER_NO)
     verify(telemetryClient, never()).trackEvent(anyString(), any(), any())
+  }
+
+  @Test
+  fun `should swallow entity not found exceptions`() {
+    whenever(keyworkerService.deallocate(OFFENDER_NO)).thenThrow(EntityNotFoundException::class.java)
+
+    complexityOfNeedEventProcessor =
+      ComplexityOfNeedEventProcessor(keyworkerService, telemetryClient, gson, "http://local")
+
+    complexityOfNeedEventProcessor.onComplexityChange(COMPLEXITY_MESSAGE_HIGH)
   }
 }
