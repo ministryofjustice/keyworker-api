b06d79fb5946d70f947e701b5a322bf61ce6538e
JamieDraper <jamie.draper@digital.justice.gov.uk>
2021-10-14 09:03:21 +0100

    NN-3712 updates after running useLatestVersions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.9"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.10"
   kotlin("plugin.spring") version "1.5.30"
   kotlin("plugin.jpa") version "1.5.30"
 }
@@ -9,8 +9,8 @@ configurations {
 
 dependencies {
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-  annotationProcessor("org.projectlombok:lombok:1.18.20")
-  testAnnotationProcessor("org.projectlombok:lombok:1.18.20")
+  annotationProcessor("org.projectlombok:lombok:1.18.22")
+  testAnnotationProcessor("org.projectlombok:lombok:1.18.22")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-cache")
   implementation("org.springframework.boot:spring-boot-starter-quartz")
@@ -21,9 +21,9 @@ dependencies {
   implementation("org.springframework:spring-webflux")
   implementation("org.springframework.boot:spring-boot-starter-reactor-netty")
   implementation("org.springframework.retry:spring-retry")
-  implementation("org.apache.camel.springboot:camel-spring-boot:3.8.0")
-  implementation("org.apache.camel:camel-quartz:3.8.0")
-  implementation("org.apache.camel:camel-direct:3.8.0")
+  implementation("org.apache.camel.springboot:camel-spring-boot:3.12.0")
+  implementation("org.apache.camel:camel-quartz:3.12.0")
+  implementation("org.apache.camel:camel-direct:3.12.0")
   implementation("org.springframework:spring-jms")
   implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.0.8")
   implementation("javax.annotation:javax.annotation-api:1.3.2")
@@ -35,27 +35,27 @@ dependencies {
   implementation("io.springfox:springfox-boot-starter:3.0.0")
   implementation("net.sf.ehcache:ehcache:2.10.9.2")
   implementation("org.apache.commons:commons-text:1.9")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.5")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.0")
   implementation("com.google.code.gson:gson:2.8.8")
-  compileOnly("org.projectlombok:lombok:1.18.20")
+  compileOnly("org.projectlombok:lombok:1.18.22")
   runtimeOnly("org.hsqldb:hsqldb:2.6.0")
-  implementation("org.flywaydb:flyway-core:7.14.1")
-  runtimeOnly("org.postgresql:postgresql:42.2.23")
-  testImplementation("org.codehaus.groovy:groovy-all:3.0.8")
+  implementation("org.flywaydb:flyway-core:8.0.1")
+  runtimeOnly("org.postgresql:postgresql:42.2.24")
+  testImplementation("org.codehaus.groovy:groovy-all:3.0.9")
   testImplementation("org.spockframework:spock-spring:2.0-groovy-3.0")
   testImplementation("org.spockframework:spock-core:2.0-groovy-3.0") {
     exclude("org.codehaus.groovy")
   }
-  testCompileOnly("org.projectlombok:lombok:1.18.20")
+  testCompileOnly("org.projectlombok:lombok:1.18.22")
   testImplementation("org.gebish:geb-core:5.0")
   testImplementation("org.gebish:geb-spock:5.0")
-  testImplementation("org.seleniumhq.selenium:selenium-support:3.141.59")
-  testImplementation("org.seleniumhq.selenium:selenium-chrome-driver:3.141.59")
-  testImplementation("org.seleniumhq.selenium:selenium-firefox-driver:3.141.59")
+  testImplementation("org.seleniumhq.selenium:selenium-support:4.0.0")
+  testImplementation("org.seleniumhq.selenium:selenium-chrome-driver:4.0.0")
+  testImplementation("org.seleniumhq.selenium:selenium-firefox-driver:4.0.0")
   testImplementation("io.github.http-builder-ng:http-builder-ng-apache:1.0.4")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("com.github.tomjankes:wiremock-groovy:0.2.0")
-  testImplementation("org.apache.camel:camel-test-spring:3.8.0")
+  testImplementation("org.apache.camel:camel-test-spring:3.12.0")
   testImplementation("com.nhaarman:mockito-kotlin-kt1.1:1.6.0")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.28.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
