1d1d1b6e70e925389b88276ed2c58006ceef9679
sp-watson <steve.watson@digital,justice.gov.uk>
2022-03-04 11:57:10 +0000

    NN-3963: Update dependencies, esp gradle-spring-boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.3-beta"
-  kotlin("plugin.spring") version "1.5.31"
-  kotlin("plugin.jpa") version "1.5.31"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.5-beta"
+  kotlin("plugin.spring") version "1.6.10"
+  kotlin("plugin.jpa") version "1.6.10"
 }
 configurations {
   implementation { exclude(mapOf("module" to "tomcat-jdbc")) }
@@ -21,9 +21,9 @@ dependencies {
   implementation("org.springframework:spring-webflux")
   implementation("org.springframework.boot:spring-boot-starter-reactor-netty")
   implementation("org.springframework.retry:spring-retry")
-  implementation("org.apache.camel.springboot:camel-spring-boot:3.8.0")
-  implementation("org.apache.camel:camel-quartz:3.8.0")
-  implementation("org.apache.camel:camel-direct:3.8.0")
+  implementation("org.apache.camel.springboot:camel-spring-boot:3.8.0") // DO NOT UPDATE - BREAKS CAMEL
+  implementation("org.apache.camel:camel-quartz:3.8.0") // DO NOT UPDATE - BREAKS CAMEL
+  implementation("org.apache.camel:camel-direct:3.8.0") // DO NOT UPDATE - BREAKS CAMEL
   implementation("org.springframework:spring-jms")
   implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.0.8")
   implementation("javax.annotation:javax.annotation-api:1.3.2")
@@ -35,28 +35,28 @@ dependencies {
   implementation("io.springfox:springfox-boot-starter:3.0.0")
   implementation("net.sf.ehcache:ehcache:2.10.9.2")
   implementation("org.apache.commons:commons-text:1.9")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.0")
-  implementation("com.google.code.gson:gson:2.8.8")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.1")
+  implementation("com.google.code.gson:gson:2.9.0")
   compileOnly("org.projectlombok:lombok:1.18.22")
-  runtimeOnly("org.hsqldb:hsqldb:2.6.0")
-  implementation("org.flywaydb:flyway-core:8.0.1")
-  runtimeOnly("org.postgresql:postgresql:42.3.2")
+  runtimeOnly("org.hsqldb:hsqldb:2.6.1")
+  implementation("org.flywaydb:flyway-core:8.5.1")
+  runtimeOnly("org.postgresql:postgresql:42.3.3")
   testImplementation("org.codehaus.groovy:groovy-all:3.0.9")
-  testImplementation("org.spockframework:spock-spring:2.0-groovy-3.0")
-  testImplementation("org.spockframework:spock-core:2.0-groovy-3.0") {
+  testImplementation("org.spockframework:spock-spring:2.2-M1-groovy-4.0")
+  testImplementation("org.spockframework:spock-core:2.2-M1-groovy-4.0") {
     exclude("org.codehaus.groovy")
   }
   testCompileOnly("org.projectlombok:lombok:1.18.22")
-  testImplementation("org.gebish:geb-core:5.0")
-  testImplementation("org.gebish:geb-spock:5.0")
-  testImplementation("org.seleniumhq.selenium:selenium-support:4.0.0")
-  testImplementation("org.seleniumhq.selenium:selenium-chrome-driver:4.0.0")
-  testImplementation("org.seleniumhq.selenium:selenium-firefox-driver:4.0.0")
+  testImplementation("org.gebish:geb-core:5.1")
+  testImplementation("org.gebish:geb-spock:5.1")
+  testImplementation("org.seleniumhq.selenium:selenium-support:4.1.2")
+  testImplementation("org.seleniumhq.selenium:selenium-chrome-driver:4.1.2")
+  testImplementation("org.seleniumhq.selenium:selenium-firefox-driver:4.1.2")
   testImplementation("io.github.http-builder-ng:http-builder-ng-apache:1.0.4")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("com.github.tomjankes:wiremock-groovy:0.2.0")
-  testImplementation("org.apache.camel:camel-test-spring:3.8.0")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.28.0")
+  testImplementation("org.apache.camel:camel-test-spring:3.15.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.32.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("org.springframework.security:spring-security-test")
 }
