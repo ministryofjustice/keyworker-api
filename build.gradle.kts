df0995a63ecf04040a83bff7110d7ce893a11473
tim <wright.tim.j@gmail.com>
2022-06-30 11:58:01 +0100

    NN-4293 bump versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.3.0"
-  kotlin("plugin.spring") version "1.6.20"
-  kotlin("plugin.jpa") version "1.6.20"
+  kotlin("plugin.spring") version "1.7.0"
+  kotlin("plugin.jpa") version "1.7.0"
 }
 configurations {
   implementation { exclude(mapOf("module" to "tomcat-jdbc")) }
