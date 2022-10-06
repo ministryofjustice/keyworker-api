986594ff3a93e70bc634f99bee6c7ed0a9f8b154
Tim Wright <wright.tim.j@gmail.com>
2022-10-06 12:16:10 +0100

    NN-4482 latest plugin version (#426)
    * NN-4482 latest plugin version
    
    * NN-4482 bump again with more suppressions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.4.3"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.4"
   kotlin("plugin.spring") version "1.7.0"
   kotlin("plugin.jpa") version "1.7.0"
 }
