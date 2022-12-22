ec34dae3dd8aa37c3e7045329391e90d467304e5
Neil Mendum <neil.mendum@digital.justice.gov.uk>
2022-12-22 15:55:25 +0000

    NN-4730: bump helm charts and deps (#429)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.7"
-  kotlin("plugin.spring") version "1.7.21"
-  kotlin("plugin.jpa") version "1.7.21"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.7.4"
+  kotlin("plugin.spring") version "1.7.22"
+  kotlin("plugin.jpa") version "1.7.22"
 }
 configurations {
   implementation { exclude(mapOf("module" to "tomcat-jdbc")) }
@@ -25,25 +25,25 @@ dependencies {
   implementation("org.apache.camel:camel-quartz:3.8.0") // DO NOT UPDATE - BREAKS CAMEL
   implementation("org.apache.camel:camel-direct:3.8.0") // DO NOT UPDATE - BREAKS CAMEL
   implementation("org.springframework:spring-jms")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.13")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.2.0")
   implementation("javax.annotation:javax.annotation-api:1.3.2")
   implementation("javax.xml.bind:jaxb-api:2.3.1")
   implementation("com.sun.xml.bind:jaxb-impl:4.0.1")
   implementation("com.sun.xml.bind:jaxb-core:4.0.1")
   implementation("javax.activation:activation:1.1.1")
   implementation("javax.transaction:javax.transaction-api:1.3")
-  implementation("io.swagger:swagger-annotations:1.6.8")
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.12")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.12")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.12")
+  implementation("io.swagger:swagger-annotations:1.6.9")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.14")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.14")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.14")
   implementation("net.sf.ehcache:ehcache:2.10.9.2")
   implementation("org.apache.commons:commons-text:1.10.0")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.14.0")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.14.1")
   implementation("com.google.code.gson:gson:2.10")
   compileOnly("org.projectlombok:lombok:1.18.24")
   runtimeOnly("org.hsqldb:hsqldb:2.7.1")
-  implementation("org.flywaydb:flyway-core:9.8.1")
-  runtimeOnly("org.postgresql:postgresql:42.5.0")
+  implementation("org.flywaydb:flyway-core:9.10.2")
+  runtimeOnly("org.postgresql:postgresql:42.5.1")
   testImplementation("org.codehaus.groovy:groovy-all:3.0.13")
   testImplementation("org.spockframework:spock-spring:2.0-groovy-3.0") // Upgrade breaks groovy
   testImplementation("org.spockframework:spock-core:2.0-groovy-3.0") { // Upgrade breaks groovy
@@ -52,9 +52,9 @@ dependencies {
   testCompileOnly("org.projectlombok:lombok:1.18.24")
   testImplementation("org.gebish:geb-core:6.0")
   testImplementation("org.gebish:geb-spock:6.0")
-  testImplementation("org.seleniumhq.selenium:selenium-support:4.6.0")
-  testImplementation("org.seleniumhq.selenium:selenium-chrome-driver:4.6.0")
-  testImplementation("org.seleniumhq.selenium:selenium-firefox-driver:4.6.0")
+  testImplementation("org.seleniumhq.selenium:selenium-support:4.7.2")
+  testImplementation("org.seleniumhq.selenium:selenium-chrome-driver:4.7.2")
+  testImplementation("org.seleniumhq.selenium:selenium-firefox-driver:4.7.2")
   testImplementation("io.github.http-builder-ng:http-builder-ng-apache:1.0.4")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("com.github.tomjankes:wiremock-groovy:0.2.0")
