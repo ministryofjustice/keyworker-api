45e0d7eb8298ff9b1695f6ef5db96ab723fe82d8
nagendramoj <139761909+nagendramoj@users.noreply.github.com>
2023-08-14 15:22:48 +0100

    NN-5031 Removed exclude logback logs. (#485)
    * NN-5031 Removed exclude logback logs.
    
    * NN-5031 Removed configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,6 @@ plugins {
   kotlin("plugin.spring") version "1.9.0"
   kotlin("plugin.jpa") version "1.9.0"
 }
-configurations {
-  implementation { exclude(mapOf("module" to "tomcat-jdbc")) }
-  implementation { exclude(mapOf("module" to "logback-classic")) }
-}
 
 // DO NOT UPDATE - BREAKS CAMEL / GROOVY
 val spockVersion = "2.4-M1-groovy-3.0"
@@ -50,13 +46,13 @@ dependencies {
     exclude("org.codehaus.groovy")
   }
   testCompileOnly("org.projectlombok:lombok")
+  testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:2.35.0")
   testImplementation("org.gebish:geb-core:$gebishVersion") // Upgrade breaks groovy
   testImplementation("org.gebish:geb-spock:$gebishVersion") // Upgrade breaks groovy
   testImplementation("org.seleniumhq.selenium:selenium-support:4.10.0")
   testImplementation("org.seleniumhq.selenium:selenium-chrome-driver:4.10.0")
   testImplementation("org.seleniumhq.selenium:selenium-firefox-driver:4.10.0")
   testImplementation("io.github.http-builder-ng:http-builder-ng-apache:1.0.4")
-  testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("com.github.tomjankes:wiremock-groovy:0.2.0")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.0.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
