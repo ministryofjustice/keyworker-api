2a7291d72d4600e1595ada96cbef2966133e17b2
nagendramoj <139761909+nagendramoj@users.noreply.github.com>
2023-09-07 11:28:55 +0100

    NN-5081 Upgraded spring boot version to 5.4.0 for security fix.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.3.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.4.0"
   kotlin("plugin.spring") version "1.9.0"
   kotlin("plugin.jpa") version "1.9.0"
 }
