abc574dd90fb7ed2df44f06e5064f85a4b03b264
tim <wright.tim.j@gmail.com>
2024-02-21 16:53:41 +0000

    remove jsonNode from filter

--- src/main/java/uk/gov/justice/digital/hmpps/keyworker/services/SubjectAccessRequestService.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/keyworker/services/SubjectAccessRequestService.kt
@@ -26,36 +26,24 @@ class SubjectAccessRequestService(
 
     val filteredRecords =
       if (fromDate != null && toDate != null) {
-        objectMapper.readTree(
-          objectMapper.writeValueAsString(
-            records.filter {
-              it.assignedDateTime.toLocalDate().isAfter(fromDate.minusDays(1)) &&
-                it.assignedDateTime.toLocalDate()
-                  .isBefore(toDate.plusDays(1))
-            },
-          ),
-        )
+        records.filter {
+          it.assignedDateTime.toLocalDate().isAfter(fromDate.minusDays(1)) &&
+            it.assignedDateTime.toLocalDate()
+              .isBefore(toDate.plusDays(1))
+        }
       } else if (fromDate != null) {
-        objectMapper.readTree(
-          objectMapper.writeValueAsString(
-            records.filter {
-              it.assignedDateTime.toLocalDate().isAfter(fromDate.minusDays(1))
-            },
-          ),
-        )
+        records.filter {
+          it.assignedDateTime.toLocalDate().isAfter(fromDate.minusDays(1))
+        }
       } else if (toDate != null) {
-        objectMapper.readTree(
-          objectMapper.writeValueAsString(
-            records.filter {
-              it.assignedDateTime.toLocalDate().isBefore(toDate.plusDays(1))
-            },
-          ),
-        )
+        records.filter {
+          it.assignedDateTime.toLocalDate().isBefore(toDate.plusDays(1))
+        }
       } else {
-        objectMapper.readTree(objectMapper.writeValueAsString(records))
+        records
       }
 
-    if (filteredRecords.isEmpty) throw NoContentFoundException()
+    if (filteredRecords.isEmpty()) throw NoContentFoundException()
 
     return objectMapper.readTree(objectMapper.writeValueAsString(filteredRecords))
   }
