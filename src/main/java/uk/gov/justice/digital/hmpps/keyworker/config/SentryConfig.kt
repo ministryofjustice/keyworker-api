bd93400dc2c5974c1a428444c85e62a1de70f009
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2025-02-13 14:43:12 +0000

    MIA-147 sentry sampling config

--- src/main/java/uk/gov/justice/digital/hmpps/keyworker/config/SentryConfig.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/keyworker/config/SentryConfig.kt
@@ -1,8 +1,10 @@
 package uk.gov.justice.digital.hmpps.keyworker.config
 
 import io.sentry.SentryOptions
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.util.regex.Pattern.matches
 
 @Configuration
 class SentryConfig {
@@ -11,4 +13,20 @@ class SentryConfig {
     SentryOptions.BeforeSendTransactionCallback { transaction, _ ->
       transaction.transaction?.let { if (it.startsWith("GET /health") or it.startsWith("GET /info")) null else transaction }
     }
+
+  @Bean
+  fun transactionSampling() = SentryOptions.TracesSamplerCallback { context ->
+    context.customSamplingContext?.let {
+      val request = it["request"] as HttpServletRequest
+      when (request.method) {
+        "GET" if (matches("/key-worker/offender/[A-Z][0-9]{4}[A-Z]{2}", request.requestURI)) -> {
+          0.0025
+        }
+
+        else -> {
+          0.05
+        }
+      }
+    }
+  }
 }
