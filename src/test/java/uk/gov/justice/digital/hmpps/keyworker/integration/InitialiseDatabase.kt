0b64fabcd89bf5168266d72a5f43de4deedb5b76
Sameer Jahangir <sameer.jahangir@justice.gov.uk>
2025-02-20 15:41:07 +0000

    Create InitialiseDatabase.kt

---
+++ src/test/java/uk/gov/justice/digital/hmpps/keyworker/integration/InitialiseDatabase.kt
@@ -0,0 +1,11 @@
+package uk.gov.justice.digital.hmpps.keyworker.integration
+
+import org.junit.jupiter.api.Test
+
+class InitialiseDatabase : IntegrationTest() {
+
+  @Test
+  fun `initialises database`() {
+    println("Database has been initialised by IntegrationTestBase")
+  }
+}
