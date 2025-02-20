48250c0fe7d2484a8ec88733d0a3e3e75a5be56f
Sameer Jahangir <sameer.jahangir@justice.gov.uk>
2025-02-20 15:54:52 +0000

    Lint fix

--- src/test/java/uk/gov/justice/digital/hmpps/keyworker/integration/InitialiseDatabase.kt
+++ src/test/java/uk/gov/justice/digital/hmpps/keyworker/integration/InitialiseDatabase.kt
@@ -3,7 +3,6 @@ package uk.gov.justice.digital.hmpps.keyworker.integration
 import org.junit.jupiter.api.Test
 
 class InitialiseDatabase : IntegrationTest() {
-
   @Test
   fun `initialises database`() {
     println("Database has been initialised by IntegrationTestBase")
