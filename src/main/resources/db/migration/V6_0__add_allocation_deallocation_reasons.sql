b2a9f01666d622a3b785e8775c4aa78ecea1288f
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2025-03-24 15:05:02 +0000

    MIA-244 Allocation and Deallocation reasons as reference data

--- src/main/resources/db/migration/V6_0__add_allocation_deallocation_reasons.sql
+++ src/main/resources/db/migration/V6_0__add_allocation_deallocation_reasons.sql
@@ -17,21 +17,34 @@ from ref_data nrd
 where not exists(select 1 from reference_data rd where rd.domain = nrd.domain and rd.code = nrd.code);
 
 alter table offender_key_worker
-    add column allocation_reason_id   bigint references reference_data (id),
-    add column deallocation_reason_id bigint references reference_data (id);
+    add column if not exists allocation_reason_id   bigint references reference_data (id),
+    add column if not exists deallocation_reason_id bigint references reference_data (id);
 
-update offender_key_worker okw
-set allocation_reason_id   = (select id
-                              from reference_data rd
-                              where rd.domain = 'ALLOCATION_REASON'
-                                and rd.code = okw.alloc_reason),
-    deallocation_reason_id = (select id
-                              from reference_data rd
-                              where rd.domain = 'ALLOCATION_REASON'
-                                and rd.code = okw.dealloc_reason)
-where allocation_reason_id is null;
+
+create or replace function update_allocation_reason_to_reference_data() returns void as
+$$
+begin
+    if exists (select 1
+               from information_schema.columns
+               where table_name = 'offender_key_worker'
+                 and column_name = 'alloc_reason') then
+        update offender_key_worker okw
+        set allocation_reason_id   = (select id
+                                      from reference_data rd
+                                      where rd.domain = 'ALLOCATION_REASON'
+                                        and rd.code = okw.alloc_reason),
+            deallocation_reason_id = (select id
+                                      from reference_data rd
+                                      where rd.domain = 'ALLOCATION_REASON'
+                                        and rd.code = okw.dealloc_reason)
+        where allocation_reason_id is null;
+    end if;
+end ;
+$$ language plpgsql;
+
+select update_allocation_reason_to_reference_data();
 
 alter table offender_key_worker
     alter column allocation_reason_id set not null,
-    drop column alloc_reason,
-    drop column dealloc_reason;
+    drop column if exists alloc_reason,
+    drop column if exists dealloc_reason;
