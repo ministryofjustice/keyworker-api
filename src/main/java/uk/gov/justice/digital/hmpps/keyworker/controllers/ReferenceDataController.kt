40141b7d30f5096126716a1d5c323adf91a73cda
bill-moj <167539377+bill-moj@users.noreply.github.com>
2025-06-03 12:25:49 +0100

    Rename keyworker-status to staff-status in ref data schema (#705)

--- src/main/java/uk/gov/justice/digital/hmpps/keyworker/controllers/ReferenceDataController.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/keyworker/controllers/ReferenceDataController.kt
@@ -27,7 +27,7 @@ class ReferenceDataController(
       schema =
         Schema(
           type = "string",
-          allowableValues = ["keyworker-status", "allocation-reason", "deallocation-reason"],
+          allowableValues = ["staff-status", "allocation-reason", "deallocation-reason"],
         ),
     )
     @PathVariable domain: String,
