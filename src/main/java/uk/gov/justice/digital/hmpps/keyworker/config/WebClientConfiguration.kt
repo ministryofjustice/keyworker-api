55ad8e8d0e08221aeb3d33cc8e6aedb64747b376
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2025-08-20 09:52:49 +0100

    add caching client service (#828)

--- src/main/java/uk/gov/justice/digital/hmpps/keyworker/config/WebClientConfiguration.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/keyworker/config/WebClientConfiguration.kt
@@ -11,7 +11,6 @@ import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction
 import org.springframework.web.reactive.function.client.ClientRequest
@@ -38,8 +37,8 @@ class WebClientConfiguration(
 ) {
   @Bean
   fun authorizedClientManager(
-    clientRegistrationRepository: ClientRegistrationRepository?,
-    oAuth2AuthorizedClientService: OAuth2AuthorizedClientService?,
+    clientRegistrationRepository: ClientRegistrationRepository,
+    oAuth2AuthorizedClientService: CachingClientService,
   ): OAuth2AuthorizedClientManager {
     val authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build()
     val authorizedClientManager =
