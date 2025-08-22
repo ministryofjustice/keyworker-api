69c136bd5ca97a36cc52885537035820e7e1bf97
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2025-08-22 16:45:38 +0100

    Chunk to 1000 limit for prisoner search (#839)

--- src/main/java/uk/gov/justice/digital/hmpps/keyworker/integration/prisonersearch/PrisonerSearchClient.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/keyworker/integration/prisonersearch/PrisonerSearchClient.kt
@@ -5,7 +5,9 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToFlux
 import org.springframework.web.reactive.function.client.bodyToMono
+import reactor.core.publisher.Flux
 import uk.gov.justice.digital.hmpps.keyworker.dto.PersonSearchRequest
 import uk.gov.justice.digital.hmpps.keyworker.integration.Prisoner
 import uk.gov.justice.digital.hmpps.keyworker.integration.Prisoners
@@ -31,18 +33,23 @@ class PrisonerSearchClient(
       .block()!!
 
   fun findPrisonerDetails(prisonNumbers: Set<String>): List<Prisoner> =
-    webClient
-      .post()
-      .uri {
-        it.path("/prisoner-search/prisoner-numbers")
-        it.queryParam("responseFields", Prisoner.fields())
-        it.build()
-      }.bodyValue(PrisonerNumbers(prisonNumbers))
-      .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-      .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
-      .retrieve()
-      .bodyToMono<List<Prisoner>>()
-      .retryRequestOnTransientException()
+    Flux
+      .fromIterable(prisonNumbers)
+      .buffer(1000)
+      .flatMap {
+        webClient
+          .post()
+          .uri {
+            it.path("/prisoner-search/prisoner-numbers")
+            it.queryParam("responseFields", Prisoner.fields())
+            it.build()
+          }.bodyValue(PrisonerNumbers(prisonNumbers))
+          .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+          .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+          .retrieve()
+          .bodyToFlux<Prisoner>()
+          .retryRequestOnTransientException()
+      }.collectList()
       .block()!!
 
   fun findFilteredPrisoners(
