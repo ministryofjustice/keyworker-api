ab35407a3c39f36f02a299087382afa444e07b3f
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2025-08-27 12:36:09 +0100

    Update script

--- src/main/resources/db/migration/V15_0__session_and_entry_by_policy.sql
+++ src/main/resources/db/migration/V15_0__session_and_entry_by_policy.sql
@@ -65,22 +65,25 @@ values ('KA', 'KS', 'KEY_WORKER',
        ('KA', 'KE', 'KEY_WORKER',
         (select id from reference_data where domain = 'RECORDED_EVENT_TYPE' and code = 'ENTRY')),
        ('REPORT', 'POE', 'PERSONAL_OFFICER',
-        (select id from reference_data where domain = 'RECORDED_EVENT_TYPE' and code = 'ENTRY'));
+        (select id from reference_data where domain = 'RECORDED_EVENT_TYPE' and code = 'ENTRY'))
+on conflict do nothing ;
 
-create index if not exists idx_recorded_event_type_id on recorded_event (type_id);
+create index if not exists idx_recorded_event_type_id on allocation_case_note (type_id);
 
-update recorded_event re
-set type_id = cntret.recorded_event_type_id
+update allocation_case_note re
+set type_id     = cntret.recorded_event_type_id,
+    policy_code = cntret.policy_code
 from case_note_type_recorded_event_type cntret
-where re.cn_type = cntret.cn_type
-  and re.cn_sub_type = cntret.cn_sub_type
+where re.type = cntret.cn_type
+  and re.sub_type = cntret.cn_sub_type
   and re.type_id is null;
 
-update recorded_event_audit rea
-set type_id = cntret.recorded_event_type_id
+update allocation_case_note_audit rea
+set type_id     = cntret.recorded_event_type_id,
+    policy_code = cntret.policy_code
 from case_note_type_recorded_event_type cntret
-where rea.cn_type = cntret.cn_type
-  and rea.cn_sub_type = cntret.cn_sub_type
+where rea.type = cntret.cn_type
+  and rea.sub_type = cntret.cn_sub_type
   and rea.type_id is null;
 
 alter table recorded_event
