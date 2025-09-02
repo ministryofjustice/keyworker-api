6088dc95c30634c46e2b334a6d02794ec717a3fd
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2025-09-02 09:14:09 +0100

    MIA-746 prison stats nullable fields (#852)
    * MIA-746 prison stats nullable fields

--- src/main/java/uk/gov/justice/digital/hmpps/keyworker/services/Statistic.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/keyworker/services/Statistic.kt
@@ -6,9 +6,9 @@ import java.math.RoundingMode.HALF_EVEN
 object Statistic {
   internal fun percentage(
     numerator: Int,
-    denominator: Int,
+    denominator: Int?,
   ): Double? =
-    if (numerator == 0 || denominator == 0) {
+    if (numerator == 0 || denominator == null || denominator == 0) {
       null
     } else {
       BigDecimal((numerator.toDouble() / denominator.toDouble()) * 100).setScale(2, HALF_EVEN).toDouble()
