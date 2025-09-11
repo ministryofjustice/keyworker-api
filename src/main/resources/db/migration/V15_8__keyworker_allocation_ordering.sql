4c27a963533d662ec3d2bdc0136833bb1c670875
Sameer Jahangir <sameer.jahangir@justice.gov.uk>
2025-09-11 13:42:14 +0100

    Create migration script to add columns to table

---
+++ src/main/resources/db/migration/V15_8__keyworker_allocation_ordering.sql
@@ -0,0 +1,19 @@
+ALTER TABLE prison_configuration ADD COLUMN IF NOT EXISTS allocation_order VARCHAR (15);
+ALTER TABLE prison_configuration ALTER COLUMN allocation_order SET DEFAULT 'BY_ALLOCATIONS';
+
+UPDATE prison_configuration SET allocation_order = 'BY_ALLOCATIONS' WHERE allocation_order IS NULL;
+
+ALTER TABLE prison_configuration ALTER COLUMN allocation_order SET NOT NULL;
+
+DO
+$$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM pg_constraint
+        WHERE conname = 'prison_config_allocation_order_valid'
+    ) THEN
+ALTER TABLE prison_configuration ADD CONSTRAINT prison_config_allocation_order_valid CHECK (allocation_order IN ('BY_ALLOCATIONS', 'BY_NAME'));
+END IF;
+END;
+$$;
