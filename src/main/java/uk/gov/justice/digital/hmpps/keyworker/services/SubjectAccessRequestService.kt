7acc6d6bd60991984fdb98f00bf8e981662942d0
Sameer Jahangir <sameer.jahangir@justice.gov.uk>
2025-09-28 23:29:25 +0100

    Create new SubjectAccessRequestService.kt and remove SubjectAccessRequestController.kt

---
+++ src/main/java/uk/gov/justice/digital/hmpps/keyworker/services/SubjectAccessRequestService.kt
@@ -0,0 +1,22 @@
+package uk.gov.justice.digital.hmpps.keyworker.services
+
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.keyworker.sar.SubjectAccessRequest
+import uk.gov.justice.hmpps.kotlin.sar.HmppsPrisonSubjectAccessRequestService
+import uk.gov.justice.hmpps.kotlin.sar.HmppsSubjectAccessRequestContent
+import java.time.LocalDate
+
+@Service
+class SubjectAccessRequestService(
+  private val subjectAccessRequest: SubjectAccessRequest
+) : HmppsPrisonSubjectAccessRequestService {
+
+  override fun getPrisonContentFor(
+    prn: String,
+    fromDate: LocalDate?,
+    toDate: LocalDate?
+  ): HmppsSubjectAccessRequestContent? {
+    val sarResponse = subjectAccessRequest.getSarContent(prn, fromDate, toDate)
+    return sarResponse?.let { HmppsSubjectAccessRequestContent(it.content) }
+  }
+}
