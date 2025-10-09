c0d44f7c067f6eedba93739a7ca5f8d992303900
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2025-10-09 16:21:16 +0100

    Update deprecated controller url and mark deprecated

--- src/main/kotlin/uk/gov/justice/digital/hmpps/keyworker/controllers/DeprecatedEndpointController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/keyworker/controllers/DeprecatedEndpointController.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.keyworker.controllers
 
+import io.swagger.v3.oas.annotations.Operation
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import org.springframework.http.ResponseEntity
@@ -14,7 +15,8 @@ import uk.gov.justice.digital.hmpps.keyworker.services.GetCurrentAllocations
 class DeprecatedEndpointController(
   private val allocations: GetCurrentAllocations,
 ) {
-  @GetMapping("/offender/{offenderNo}")
+  @Operation(deprecated = true)
+  @GetMapping("/key-worker/offender/{offenderNo}")
   fun deprecatedGetOffendersKeyworker(
     @PathVariable("offenderNo") personIdentifier: String,
   ): ResponseEntity<BasicKeyworkerInfo> =
