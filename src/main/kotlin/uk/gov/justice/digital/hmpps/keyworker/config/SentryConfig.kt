2ecad581e5c2f0d8700c1790be3c9499034fdb2f
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2025-10-17 09:58:09 +0100

    undefined prison returns 200

--- src/main/kotlin/uk/gov/justice/digital/hmpps/keyworker/config/SentryConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/keyworker/config/SentryConfig.kt
@@ -46,8 +46,7 @@ class SentryConfig {
     (this is EntityNotFoundException) or ((this as? WebClientResponseException)?.statusCode?.is4xxClientError ?: false)
 
   private fun String.isHighUsage(): Boolean =
-    matches("/key-worker/offender/[A-Z][0-9]{4}[A-Z]{2}", this) or
-      matches("/prisons/[A-Z]{3}/prisoners/[A-Z][0-9]{4}[A-Z]{2}/keyworkers/current", this) or
+    matches("/prisons/[A-Z]{3}/prisoners/[A-Z][0-9]{4}[A-Z]{2}/keyworkers/current", this) or
       matches("/prisons/[A-Z]{3}/staff/\\d+/job-classifications", this) or
       matches("/prisoners/[A-Z][0-9]{4}[A-Z]{2}/allocations/current", this)
 }
