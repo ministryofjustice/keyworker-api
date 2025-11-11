2c35c0677173920e040a766e7d7aca74cc5d1755
Sameer Jahangir <sameer.jahangir@justice.gov.uk>
2025-11-11 14:31:40 +0000

    Add Swagger doc to GetAllocationsController

--- src/main/kotlin/uk/gov/justice/digital/hmpps/keyworker/controllers/GetAllocationsController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/keyworker/controllers/GetAllocationsController.kt
@@ -1,5 +1,11 @@
 package uk.gov.justice.digital.hmpps.keyworker.controllers
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
@@ -10,17 +16,50 @@ import uk.gov.justice.digital.hmpps.keyworker.config.MANAGE_ALLOCATIONS
 import uk.gov.justice.digital.hmpps.keyworker.config.PolicyHeader
 import uk.gov.justice.digital.hmpps.keyworker.model.person.StaffAllocationHistory
 import uk.gov.justice.digital.hmpps.keyworker.services.GetAllocations
+import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
 
 @RestController
 @RequestMapping("/prisoners/{prisonNumber}")
 class GetAllocationsController(
   private val allocations: GetAllocations,
 ) {
+  @Operation(
+    summary = "Retrieves allocation hisotry for a given prisoner",
+    description = "Retrieves current allocations for a given prisoner")
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Allocation history returned."
+      ),
+      ApiResponse(
+        responseCode = "400",
+        description = "Bad request.",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))]
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorised, requires a valid Oauth2 token",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden, requires an appropriate role",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "The allocation history associated with this identifier was not found.",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ]
+  )
   @PolicyHeader
   @Tag(name = MANAGE_ALLOCATIONS)
   @PreAuthorize("hasRole('${Roles.ALLOCATIONS_UI}')")
   @GetMapping("/allocations")
   fun getAllocations(
+    @Parameter(required = true, example = "A12345", description = "The prisoners identifier.")
     @PathVariable prisonNumber: String,
   ): StaffAllocationHistory = allocations.historyFor(prisonNumber)
 }
