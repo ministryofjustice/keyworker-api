2ae663fa5f0e62731e533805ffde6bd69f49a566
Sameer Jahangir <sameer.jahangir@justice.gov.uk>
2025-11-12 13:05:06 +0000

    Add Swagger doc to ReferenceDataController

--- src/main/kotlin/uk/gov/justice/digital/hmpps/keyworker/controllers/ReferenceDataController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/keyworker/controllers/ReferenceDataController.kt
@@ -1,7 +1,11 @@
 package uk.gov.justice.digital.hmpps.keyworker.controllers
 
+import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
@@ -13,6 +17,7 @@ import uk.gov.justice.digital.hmpps.keyworker.config.REFERENCE_DATA
 import uk.gov.justice.digital.hmpps.keyworker.domain.ReferenceDataDomain
 import uk.gov.justice.digital.hmpps.keyworker.model.CodedDescription
 import uk.gov.justice.digital.hmpps.keyworker.services.RetrieveReferenceData
+import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
 
 @Tag(name = REFERENCE_DATA)
 @RestController
@@ -20,6 +25,33 @@ import uk.gov.justice.digital.hmpps.keyworker.services.RetrieveReferenceData
 class ReferenceDataController(
   val referenceData: RetrieveReferenceData,
 ) {
+  @Operation(
+    summary = "Retrieve reference data for a specific domain.",
+    description = "Retrieve reference data for a specific domain."
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Reference returned"
+      ),
+      ApiResponse(
+        responseCode = "400",
+        description = "Bad request.",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))]
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorised, requires a valid Oauth2 token",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden, requires an appropriate role",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      )
+    ]
+  )
   @PolicyHeader
   @PreAuthorize("hasRole('${Roles.ALLOCATIONS_UI}')")
   @GetMapping
