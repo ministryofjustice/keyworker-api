8351a08fa28e0f6cd9a2587ce435eb3054af2385
Chux <chukwudi.ugorji@digital.justice.gov.uk>
2026-03-09 12:20:53 +0000

    bump versions up

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,10 @@ plugins {
 
 dependencies {
 
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.1")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-webclient")
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.0.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.0.2")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:7.0.1")
 
   implementation("io.sentry:sentry-spring-boot-4:8.32.0")
